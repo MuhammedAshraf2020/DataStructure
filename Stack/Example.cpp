6e925c1a04c0f4a0f2ca8a417c92eb7f00e0c12f
drrobotA <53389411+drrobotA@users.noreply.github.com>
2020-04-14 08:31:32 +0000

    Update Example.cpp

--- Stack/Example.cpp
+++ Stack/Example.cpp
@@ -1,1 +1,15 @@
+#include<iostream>
+#include "SSS.h"
+using namespace std;
+int main()
+{
+    Stack S;
+    int var;
+    CreateStack(&S);
+    push(20 , &S);
+    pop(&var ,&S);
+    cout<<"POP here: "<<var<<endl;
+    return 0;
+}
+
 
